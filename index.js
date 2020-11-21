d96efa149d0c600c0a76aaf19bbbdfbe44ffb2b1
erikoui <eriksofs@hotmail.com>
2020-11-21 22:43:54 +0200

    upload test

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // Load dependencies
 const express = require('express');
-const {db}=require('./db');
+const {db} = require('./db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const _ = require('lodash');
 
 // Server port to listen on
 const PORT = process.env.PORT || 5000;
 app = express();
 // ------------ init middlewares ------------
 // Request logger
-app.use(require('morgan')('combined'));
+
 // Accepting json bodies (🔥hot🔥)
-app.use(require('body-parser').json({type: 'application/json'}));
+app.use(bodyParser.json({type: 'application/json'}));
+// Also accepting file uploads
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload({
+  createParentPath: true,
+  limits: {
+    fileSize: 2 * 1024 * 1024 * 1024, // 2MB max file(s) size
+  },
+}));
+app.use(cors());
+app.use(require('morgan')('combined'));
 
 // ------------ API--------------
 app.get('/test', (req, res) => {
@@ -18,35 +32,61 @@ app.get('/test', (req, res) => {
     message: 'hello world',
   });
 });
-app.get('/allposts', (req, res)=>{
-  db.posts.all().then((data)=>{
+app.get('/allposts', (req, res) => {
+  db.posts.all().then((data) => {
     res.json(data);
   });
 });
-app.get('/replies', (req, res)=>{
-  const postId=req.query.postid;
+app.get('/replies', (req, res) => {
+  const postId = req.query.postid;
   if (postId) {
-    db.posts.getReplies(postId).then((data)=>{
+    db.posts.getReplies(postId).then((data) => {
       res.json({
         err: false,
         ...data,
       });
-    }).catch((e)=>{
-      res.json({
-        err: true,
-        message: e.message,
-        trace: e,
-      });
+    }).catch((e) => {
+      res.status(500).send(e);
     });
   } else {
-    res.json({
-      err: true,
-      message: 'No post id specified',
-    });
+    res.status(500).send(new Error('No post id specified'));
   }
 });
-app.post('/newpost', (req, res)=>{
-  res.end('asd');
+app.post('/newpost', async (req, res) => {
+  try {
+    if (!req.files) {
+      res.json({
+        err: true,
+        message: 'No file uploaded',
+      });
+    } else {
+      const data = [];
+
+      // loop all files
+      _.forEach(_.keysIn(req.files.images), (key) => {
+        const img = req.files.images[key];
+
+        // move photo to uploads directory
+        img.mv('./uploads/' + img.name);
+
+        // push file details
+        data.push({
+          name: photo.name,
+          mimetype: photo.mimetype,
+          size: photo.size,
+        });
+      });
+
+      // return response
+      res.json({
+        err: false,
+        message: 'Files are uploaded',
+        data: data,
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 
